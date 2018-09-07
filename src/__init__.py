1e08baf387a44d6d3a87b9a4d8f3441b28b65611
Bianca Schoene <schoene@dkms-lab.de>
2018-09-07 09:07:50 +0200

    added __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env Python2
+'''
+
+
+'''
+__author__ = "Vineeth Surendranath"
+__copyright__ = "Copyright 2016, DKMS Life Science Lab GmbH"
+__license__ = "MIT"
+__maintainer__ = "Vineeth Surendranath"
+__email__ = "surendranath@dkms-lab.de"
+
+__version__ = "1.0.0"
+__status__ = "productive"
