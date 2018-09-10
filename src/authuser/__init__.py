77891bf9c0bc7e20dd26b050deddaed2cc91f13d
Bianca Schoene <schoene@dkms-lab.de>
2018-09-10 10:33:35 +0200

    added files for submodule authuser

---
+++ src/authuser/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env Python3
+'''
+
+
+'''
+__author__ = "Vineeth Surendranath"
+__copyright__ = "Copyright 2018, DKMS Life Science Lab GmbH"
+__license__ = "GPL"
+__maintainer__ = "Vineeth Surendranath"
+__email__ = "surendranath@dkms-lab.de"
+
+__version__ = "0.1.0"
+__status__ = "testing"
+__all__ = ["authuser"]
