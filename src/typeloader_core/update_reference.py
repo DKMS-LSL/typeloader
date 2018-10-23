7ab5a9ba73431b03d55f9ea5af5920bd289bb230
Bianca Schoene <schoene@dkms-lab.de>
2018-10-23 12:40:16 +0200

    added logging and got it working (#4)

--- src/typeloader_core/update_reference.py
+++ src/typeloader_core/update_reference.py
@@ -1,7 +1,14 @@
+"""
+update_referce.py
+
+handles updating of the reference data for TypeLoader
+"""
 from os import path
-import shutil
+import re, shutil
 import urllib.request
 import hashlib
+
+import hla_embl_parser
 
 remote_db_path = { \
             "hla_path" : "https://github.com/ANHIG/IMGTHLA/raw/Latest/hla.dat", \
@@ -11,58 +18,169 @@ remote_db_path = { \
 remote_checksumfile_index = { \
             "kir_checksums_file" : "https://raw.githubusercontent.com/ANHIG/IPDKIR/Latest/md5checksum.txt", \
             "hla_checksums_file" : "https://raw.githubusercontent.com/ANHIG/IMGTHLA/Latest/md5checksum.txt" \
-             }    
+            }
 
+reference_local_path = r"H:\Mitarbeiterordner\Bianca\Eclipse-Workspaces\Python3\Python3\src\typeloader2\src\reference_data2"
 
-def get_remote_md5checksum(db_name):
-
+def get_remote_md5checksum(db_name, log):
+    """retrieves MD5 checksum from IPD's checksum_file 
+    """
+    log.debug("\tGetting checksum of current remote file...")
     remote_checksumfile = remote_checksumfile_index["%s_checksums_file" % db_name]
-    checksum_response = urllib.request.urlopen(remote_checksumfile)
+    checksum_response = urllib.request.urlopen(remote_checksumfile, timeout=5)
     checksum_data = checksum_response.read().decode("utf-8") 
 
     # The checksum_data are in lines of the form MD5 (hla.dat) = 2dde3a26abf52c11a70aae7fa8f14666\n
-    datFile_regex = re.compile("((\(%s\.dat\)) = (.*?)\n)" % db_name)
-
-    return datFile_regex.expand("\\3")
-
-
-def get_local_md5checksum(db_name):
-
-    if db_name == "kir": 
-        use_dbname = "KIR"  # biological databases and consistency in naming are arch enemies 
+    pattern = ".*%s.dat\) = (.*)\n" % db_name
+    datFile_regex = re.compile(pattern)
+    match = datFile_regex.search(checksum_data)
+    if match:
+        md5 = match.groups()[0]
+        log.debug("\t=> {}".format(md5))
     else:
-        use_dbname = db_name
+        md5 = None
+        msg = "Could not find MD5 checksum of remote {} file!".format(db_name)
+        log.error(msg)
+    return md5
 
+
+def get_local_md5checksum(use_dbname, reference_local_path, log):
+    """gets the MD5 checksum of the current local database version
+    """
+    log.debug("\tGetting checksum of local file...")
     
-    local_reference_file = path.join(reference_location, "%s.dat" % use_dbname)
+    local_reference_file = path.join(reference_local_path, "%s.dat" % use_dbname)
     
-    return hashlib.md5(open(local_reference_file, "rb").read()).hexdigest()
+    md5 = hashlib.md5(open(local_reference_file, "rb").read()).hexdigest()
+    log.debug("\t=> {}".format(md5))
+    return md5
 
 
-def update_databases(reference_local_path):
+def create_reference_files(db_name, log):
+    
+    
 
+def update_databases(log):
+    """checks IPD Github account for new releases,
+    if found, downloads them and creates TypeLoader's reference files
+    """
+    log.info("Checking IPD for new releases...")
     db_list = ["hla", "kir"]
+    updated = []
 
-    if db_name == "kir": 
-        use_dbname = "KIR"  # biological databases and consistency in naming are arch enemies 
-    else:
-        use_dbname = db_name
-    
     for db_name in db_list:
-        if get_remote_md5checksum(db_name) == get_local_md5checksum(db_name): 
+        log.info("Checking {}...".format(db_name.upper()))
+        if db_name == "kir": 
+            use_dbname = "KIR"  # biological databases and consistency in naming are arch enemies 
+        else:
+            use_dbname = db_name
+            
+        remote_md5 = get_remote_md5checksum(db_name, log)
+        if not remote_md5:
+            log.info("Aborting attempt to update the {} reference".format(db_name))
             continue
-
+        local_md5 = get_local_md5checksum(use_dbname, reference_local_path, log)
+        if remote_md5 == local_md5:
+            log.info("\t=> {} is up to date".format(db_name)) 
+            continue
         
+        log.info("=> Found new reference version for {}".format(db_name))
+        log.debug("\tdownloading new file...")
         remote_db_file = remote_db_path["%s_path" % db_name]
-        local_db_file = path.join(reference_location, "%s.dat" % use_dbname)
-        with urllib.request.urlopen(remote_db_file) as db_response, open(local_db_file, "w") as db_local:
-            shutile.copyfileobj(db_response, db_local)
+        local_db_file = path.join(reference_local_path, "%s.dat" % use_dbname)
+        with urllib.request.urlopen(remote_db_file, timeout = 10) as db_response, open(local_db_file, "wb") as db_local:
+            shutil.copyfileobj(db_response, db_local)
 
-    return 
+        log.debug("\t => successfully downloaded new {} file".format(db_name))
+        updated.append(db_name.upper())
+    
+    update_msg = "IPD has released a new version of {}. I have updated my reference data accordingly.".format(" and ".join(updated))
+    log.info(update_msg)
+    return update_msg
 
 
+def start_log(include_lines = False, error_to_email = False, info_to_file = False,
+              debug_to_file = False,
+              elaborate = False, level = "DEBUG"):
+    """starts a logger and returns it for logging,
+    if log file is wanted, info_to_file should be the log file destination (path/filename);
+    default logging level is set to DEBUG
+    """
+    import logging, socket, os
+    
+    script_name = os.path.basename(__file__)
+    log = logging.getLogger(script_name)
+    
+    # set level:
+    level = level.upper()
+    if level == "DEBUG":
+        log.setLevel(logging.DEBUG)
+    elif level == "INFO":
+        log.setLevel(logging.INFO)
+    elif level == "WARNING":
+        log.setLevel(logging.WARNING)
+    elif level == "ERROR":
+        log.setLevel(logging.ERROR)
+    elif level == "CRITICAL":
+        log.setLevel(logging.CRITICAL)
+    else:
+        log.setLevel(logging.DEBUG)
+        log.warning("Unknown loglevel '%s'! Using DEBUG instead..." % level)
+    
+    # define logging format:
+    if include_lines:
+        formatter = logging.Formatter('%(levelname)s [%(asctime)s] - [ln.%(lineno)d] %(message)s')
+    elif elaborate:
+        formatter = logging.Formatter("[%(asctime)s] %(levelname)s [%(filename)s:%(lineno)s - %(funcName)s] %(message)s")
+    else:
+        formatter = logging.Formatter('%(levelname)s [%(asctime)s] - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
         
+    # establish stream handler (instead of print):
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    log.addHandler(stream_handler)
+    
+    # establish email handler, if needed: (level ERROR)  
+    if error_to_email:    
+        myport = 25
+        myserver = '192.168.2.23'
+        nodename = socket.gethostname()
+        email_adresses = ['schoene@dkms-lab.de']
+        email_handler = logging.handlers.SMTPHandler(mailhost=(myserver, myport),
+                                               fromaddr='%s@%s' % (script_name, nodename),
+                                               toaddrs=email_adresses,
+                                               subject='Error in %s' % script_name)
+        email_handler.setFormatter(formatter)
+        email_handler.setLevel(logging.ERROR)
+        log.addHandler(email_handler)
+    
+    # establish file handler, if needed: (level INFO)
+    if info_to_file:
+        file_handler = logging.FileHandler(info_to_file)
+        file_handler.setFormatter(formatter)
+        file_handler.setLevel(logging.INFO)
+        log.addHandler(file_handler)
+    
+    if debug_to_file:
+        log.debug_handler = logging.FileHandler(debug_to_file)
+        log.debug_handler.setFormatter(formatter)
+        log.debug_handler.setLevel(logging.DEBUG)
+        log.addHandler(log.debug_handler)
+    
+    return log
 
+pass
+#===========================================================
+# main:
+def main():
+    log = start_log(level="DEBUG")
+    log.info("<Start>")
+    update_databases(log)
+    log.info("<End>")
+
+
+if __name__ == '__main__':
+    main()
     
 
     
