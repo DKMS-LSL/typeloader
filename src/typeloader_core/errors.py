49b0860389583989aa1b148506c8e443d3e72345
Bianca Schoene <schoene@dkms-lab.de>
2019-03-20 12:00:50 +0100

    added MissingUTRError, downgraded IncompleteSequenceError to Warning (#63)

--- src/typeloader_core/errors.py
+++ src/typeloader_core/errors.py
@@ -6,12 +6,26 @@ contains custom exception classes
 @author: schoene
 '''
 
-class IncompleteSequenceError(Exception):
+class IncompleteSequenceWarning(Exception):
     """raised when TypeLoader encounters a sequence without a complete 3'UTR
     """
     def __init__(self, missing_bp):
         self.missing_bp = missing_bp
-        self.msg = "This sequence misses the first {} bp!\nTypeLoader requires the full 5' UTR to be included in the sequence.".format(missing_bp)
+        self.msg = "This sequence misses the first {} bp!\nDo you want to upload it anyway?\n(Note that IPD requires at least 1 bp per UTR to be contained in genomic sequences.)\n\n".format(missing_bp)
+        self.msg += "Please consider carefully: this might lead to an incorrect closest allele being selected. Also, submitting incomplete sequences decreases the quality of the official IPD databases."
+
+
+class MissingUTRError(Exception):
+    """raised when TypeLoader encounters a sequence with at least one completely missing UTR
+    """
+    def __init__(self, UTR):
+        """UTR should be 3 or 5 (int)
+        """
+        self.UTR = UTR
+        if not UTR in [3,5]:
+            raise ValueError("UTR must be given as integer 3 or 5!")
+        self.msg = "This sequence completely misses the {}' UTR!\nTypeLoader requires at least 1 bp of each UTR to be included in genomic sequences in order to comply with IPD's requirements.".format(self.UTR)
+
 
 class BothAllelesNovelError(Exception):
     """raised when TypeLoader tries to create an IPD file for an allele where both alleles of this locus are novel
@@ -21,6 +35,7 @@ class BothAllelesNovelError(Exception):
         self.allele = allele #TargetAllele object
         self.alleles = alleles # list of both alleles from the pretypings csv
         self.problem = "Cannot tell which novel allele from pretyping this is"
+        
         
 class InvalidPretypingError(Exception):
     """raised when TypeLoader tries to create an IPD file for an allele with an invalid pretyping
