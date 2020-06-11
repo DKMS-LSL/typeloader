c2265432bfe10e87f66ba72f27f2e9e1f0124e0d
Bianca Schöne <schoene@dkms-lab.de>
2020-06-11 11:55:13 +0200

    Show full DevianceError msg if allele cannot be added with restricted db (#149)

--- src/GUI_forms_new_allele.py
+++ src/GUI_forms_new_allele.py
@@ -372,6 +372,8 @@ class NewAlleleForm(CollapsibleDialog):
                                                      QMessageBox.Yes | QMessageBox.No,
                                                      QMessageBox.No)
                         if reply == QMessageBox.No:
+                            QMessageBox.information(self, "Allele too divergent",
+                                                    results[2])
                             self.dialog.close()
                             return
                         else:
