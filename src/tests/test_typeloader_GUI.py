2beeeee9a9ff2ff879c655ab0f3a1c04be29ba10
Bianca Schöne <schoene@dkms-lab.de>
2020-07-21 16:43:28 +0200

    add skip_other_tests check to TestUpdateReference

--- src/tests/test_typeloader_GUI.py
+++ src/tests/test_typeloader_GUI.py
@@ -2817,40 +2817,15 @@ class TestHomozygousXML(unittest.TestCase):
         self.assertEqual(len(diff_ena_files["deleted_sings"]), 0)
 
 
-class TestCleanStuff(unittest.TestCase):
-    """
-    Remove all directories and files written by  all unit tests
-    """
-
-    @classmethod
-    def setUpClass(self):
-        if skip_other_tests:
-            self.skipTest(self, "Skipping final cleanup because skip_other_tests is set to True")
-
-    @classmethod
-    def tearDownClass(self):
-        typeloader_GUI.close_connection(log, mydb)
-
-    def test_clean_everything(self):
-        os.remove(os.path.join(mypath_inner, "typeloader_GUI.py"))
-
-        if delete_all_stuff_at_the_end:
-            delete_written_samples(True, "ALLELES", log)
-            delete_written_samples(True, "FILES", log)
-            delete_written_samples(True, "SAMPLES", log)
-            delete_written_samples(True, "PROJECTS", log)
-            delete_written_samples(True, "ENA_SUBMISSIONS", log)
-            delete_written_samples(True, "IPD_SUBMISSIONS", log)
-
-            shutil.rmtree(os.path.join(curr_settings["projects_dir"], project_name))
-
-
 class TestUpdateReference(unittest.TestCase):
     """test whether reference updates work
     """
 
     @classmethod
     def setUpClass(self):
+        if skip_other_tests:
+            self.skipTest(self, "Skipping reference update test because skip_other_tests is set to True")
+
         self.form = GUI_mini_dialogs.RefreshReferenceDialog(curr_settings, log, None)
         self.target = "KIR"
         self.reference_local_path = os.path.join(curr_settings["root_path"],
