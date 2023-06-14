5fbac36addb0a45b00305d3b9ae7bf84a327f889
Bianca Schöne <schoene@dkms-lab.de>
2023-06-14 13:02:17 +0200

    release: version bump to 2.14.0 [TL2-268]

--- typeloader2/__init__.py
+++ typeloader2/__init__.py
@@ -9,7 +9,7 @@ __license__ = "MIT"
 __maintainer__ = "Bianca Schoene"
 __email__ = "schoene@dkms-lab.de"
 
-__version__ = "2.14.0-RC4"
+__version__ = "2.14.0"
 __status__ = "productive"
 __all__ = ["__init__",
            "db_external",
